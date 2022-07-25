198dab055aba05e8e681eb4dce68b9b4b50ff4a4
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-07-25 12:32:29 +0300

    Initial Commit

---
+++ project22-Detect-a-Beacon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project22-Detect-a-Beacon
+//
+//  Created by Nikita  on 7/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
