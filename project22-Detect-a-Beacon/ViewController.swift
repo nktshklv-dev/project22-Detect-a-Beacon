1d3af4fe2e9ca13fe1800641880d771e6ff663a0
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-07-26 13:54:23 +0300

    done

--- project22-Detect-a-Beacon/ViewController.swift
+++ project22-Detect-a-Beacon/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     
     @IBOutlet var distanceLabel: UILabel!
-    var locationManager: CLLocationManager?
+    var locationManager: CLLocationManager!
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager = CLLocationManager()
@@ -25,9 +25,46 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         if status == .authorizedAlways{
             if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self){
                 if CLLocationManager.isRangingAvailable(){
-                    // do stuff
+                    startScanning()
                 }
             }
+        }
+    }
+    
+    
+    func startScanning(){
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(uuid: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.startRangingBeacons(in: beaconRegion)
+    }
+    
+    func update(distance: CLProximity){
+        UIView.animate(withDuration: 1) { [unowned self] in
+            switch distance {
+            case .immediate:
+                self.view.backgroundColor = .red
+                self.distanceLabel.text = "RIGHT HERE"
+            case .near:
+                self.view.backgroundColor = .orange
+                self.distanceLabel.text = "NEAR"
+            case .far:
+                self.view.backgroundColor = .blue
+                self.distanceLabel.text = "FAR"
+            case .unknown:
+                self.view.backgroundColor = .gray
+                self.distanceLabel.text = "UNKNOWN"
+                
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if let beacon = beacons.first{
+            update(distance: beacon.proximity)
+        }
+        else {
+            update(distance: .unknown)
         }
     }
 
